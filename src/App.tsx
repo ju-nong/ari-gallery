178846d2750f4fcce04c2cd560b379b01d8911ff
이준용 <iiiuooyoo@gmail.com>
2023-03-09 11:50:16 +0900

    style : 문구 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { isDev } from "./utils/mode";
+import { isDev, isPrd } from "./utils/mode";
 
 function App() {
     return (
         <div className="App">
+            현재 여기는 {isPrd() ? `상용` : `개발`}입니다
+            <br />
+            <br />
             개발이면 vite 이미지가 보이고
             <br />
             상용이면 react 이미지가 보여요
